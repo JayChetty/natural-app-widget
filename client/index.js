1257996debf1911a285a8541d2d9e47bdc50b989
JayChetty <jaypaulchetty@gmail.com>
2015-12-19 11:41:38 +0000

    working version

--- client/index.js
+++ client/index.js
@@ -1,5 +1,14 @@
-SunClockFactory = require('sun-clock');
+canvasClock = require('./canvas_clock');
 NaturalCalendar = require('natural_calendar');
 window.onload = function(){
-  console.log('in the house');
+  console.log('in the house yo');
+  var canvas = document.getElementById('main');
+  console.log('canvas', canvas)
+  var clock = canvasClock({
+    center: { x: canvas.width/2, y: canvas.height/2 },
+    canvas: canvas,
+    radius:50
+  });
+  clock.drawOutline();
+  clock.drawLineForTime(new Date());
 };
--- public/bundle.js
+++ public/bundle.js
@@ -44,10 +44,19 @@
 /* 0 */
 /***/ function(module, exports, __webpack_require__) {
 
-	SunClockFactory = __webpack_require__(1);
+	canvasClock = __webpack_require__(4);
 	NaturalCalendar = __webpack_require__(2);
 	window.onload = function(){
-	  console.log('in the house');
+	  console.log('in the house yo');
+	  var canvas = document.getElementById('main');
+	  console.log('canvas', canvas)
+	  var clock = canvasClock({
+	    center: { x: canvas.width/2, y: canvas.height/2 },
+	    canvas: canvas,
+	    radius:50
+	  });
+	  clock.drawOutline();
+	  clock.drawLineForTime(new Date());
 	};
 
 /***/ },
@@ -289,6 +298,86 @@
 	}
 
 	module.exports = Cycle;
+
+/***/ },
+/* 4 */
+/***/ function(module, exports, __webpack_require__) {
+
+	SunClockFactory = __webpack_require__(1);
+	module.exports = function(options){
+	  var sunClock = null;//SunClockFactory.createSunClock();
+	  console.log('sunclock', sunClock)
+	  options = options || {};
+	  console.log('options', options)
+	  var canvas = options.canvas;
+	  var ctx = canvas.getContext("2d");
+	  var clock = {
+	    sunClock: sunClock,
+	    radius: options.radius,
+	    center: options.center,
+	    render: function(){
+	      this.drawOutline();
+	    },
+	    drawSweep: function(startTime, endTime, color){
+	      ctx.beginPath();
+	      ctx.fillStyle = "rgba(10, 255, 99, 0.2)";
+	      ctx.moveTo(this.center.x,this.center.y);
+	      ctx.arc( 
+	        this.center.x,
+	        this.center.y,
+	        this.radius, 
+	        this.angleForTime(startTime), 
+	        this.angleForTime(endTime)
+	      );
+	      ctx.moveTo(this.center.x,this.center.y);
+	      ctx.fill();
+	    },
+	    drawOutline: function(){     
+	      ctx.beginPath();
+	      ctx.arc(this.center.x,this.center.y,this.radius,0, 2*Math.PI);
+	      ctx.stroke();          
+	    },
+	    fractionOfDay:function(time){
+	      var minutesInDay = 24 * 60;
+	      var timeInMinutes = (time.getHours() * 60) + time.getMinutes();
+	      return(timeInMinutes / minutesInDay);
+	    },
+	    angleForTime:function(time){
+	      return this.angleForFraction( this.fractionOfDay(time) );
+	    },
+	    angleForFraction:function(fraction){
+	      return( Math.PI*2 * fraction - Math.PI/2 );
+	    },
+	    drawLineForTime:function(time){
+	      this.drawLineForFraction( this.fractionOfDay(time) );
+	    },
+	    drawLineForFraction:function(fraction){
+	      console.log("draw line for fraction", fraction)
+	      ctx.beginPath();
+	      ctx.moveTo(this.center.x,this.center.y);
+	      var endPoint = this.pointOnOutline(fraction);
+	      console.log('endpoint', endPoint);
+	      ctx.lineTo(endPoint.x, endPoint.y);
+	      ctx.stroke();
+	    },
+	    pointOnOutline:function(fraction){
+	      var angle = this.angleForFraction(fraction);
+	      var unadjustedCenterPoint = this.polarToCartesian(this.radius, angle);
+	      return {
+	        x: unadjustedCenterPoint.x + this.center.x,
+	        y: unadjustedCenterPoint.y + this.center.y
+	      }
+	    },
+	    polarToCartesian:function(radius, angle){
+	      return { 
+	        x:radius*Math.cos(angle),
+	        y:radius*Math.sin(angle)
+	      }
+	    }
+
+	  }
+	  return clock;
+	}
 
 /***/ }
 /******/ ]);
